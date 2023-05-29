118dfbbb010a8c99fcc81a5151b98008bd111369
Leo <leonardocastiglione@hotmail.com>
2023-05-29 20:34:40 -0300

    Add test controller

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,10 +1,38 @@
 ﻿"""Test unitarios para pprocess"""
 import unittest
 
+from pprocess.pprocess import ParallelProcess
+from tests.utils import TestController
 
-class LoggerTestCase(unittest.TestCase):
-    """_summary_
+def method(value):
+    return value*3
+class ParallelProcessTestCase(unittest.IsolatedAsyncioTestCase):
+    '''Clase de prueba para ParallelProcess
+    '''
 
-    Args:
-        unittest (_type_): _description_
-    """
+    def __init__(self, methodName="runTest"):
+        '''Inicializador de la clase EventBusTestCase.
+        '''
+        super().__init__(methodName=methodName)
+        self.p_process = None
+
+    async def asyncSetUp(self):
+        '''Tareas asincrónas que se ejcutan antes de cada prueba.
+        '''
+        test_controller = TestController()
+        self.p_process = ParallelProcess(controller=test_controller)
+        await self.p_process.start()
+
+    async def asyncTearDown(self):
+        '''Tareas asincrónas que se ejcutan después de cada prueba.
+        '''
+        await self.p_process.close()
+
+    async def test_start(self):
+        """_summary_
+        """
+
+
+        result = await self.p_process.exe_task({'method': method, 'input': 3})
+        print("termine la tarea")
+        print(result)
---
+++ tests/utils.py
@@ -0,0 +1,18 @@
+﻿"""_summary_"""
+
+from pprocess.controller import Controller
+
+
+class TestController(Controller):
+    """_summary_
+    """
+
+    @classmethod
+    def execute(cls, params):
+        results = {}
+        for param in params:
+            method = param['method']
+            input_data = param['input']
+            results[param['id']]= method(input_data)
+        print(results)
+        return results
