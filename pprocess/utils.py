31091e742a94aefc45cfc55eb64587e4b9c6f275
lcastiglione <leonardocastiglione@hotmail.com>
2023-05-29 06:57:39 -0300

    Add util methods

---
+++ pprocess/utils.py
@@ -0,0 +1,12 @@
+﻿"""Funciones utils del proyecto"""
+
+import uuid
+
+
+def get_uid() -> str:
+    """Genera un id único
+
+    Returns:
+        (str): id único
+    """
+    return str(uuid.uuid4())
