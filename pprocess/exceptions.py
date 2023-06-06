b6b687ef2a0844a90cf1ed290d5b5decf10c9593
lcastiglione <leonardocastiglione@hotmail.com>
2023-06-06 09:08:41 -0300

    Add UserProcessExcpetion

--- pprocess/exceptions.py
+++ pprocess/exceptions.py
@@ -4,7 +4,7 @@ Módulo que contiene los tipos de excepciones que pueden ocurrir en el método d
 from logs.logger import logger
 
 
-class ResponseException(Exception):
+class ResponseProcessException(Exception):
     """_summary_
 
     Args:
@@ -14,3 +14,14 @@ class ResponseException(Exception):
     def __init__(self, msg):
         logger.error("Se produjo un error al procesar la tarea: %s", msg, exc_info=True)
         super().__init__(f"Se produjo un error al procesar la tarea: {msg}")
+
+class UserProcessException(Exception):
+    """_summary_
+
+    Args:
+        Exception (_type_): _description_
+    """
+
+    def __init__(self):
+        logger.error("Se corto la ejecucion de la tarea", exc_info=True)
+        super().__init__("Se corto la ejecucion de la tarea")
