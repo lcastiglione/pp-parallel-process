553a993bb23f02e60d3ace70086cd4575b2d9746
lcastiglione <leonardocastiglione@hotmail.com>
2023-06-12 07:22:57 -0300

    Add abstract class implementations

--- tests/utils.py
+++ tests/utils.py
@@ -22,26 +22,38 @@ class TestController(Worker):
         pass
 
     @classmethod
+    def start_process(cls, process_id: int):
+        pass
+
+    @classmethod
+    def stop_process(cls, process_id: int):
+        pass
+
+    @classmethod
+    def print_error(cls, error_message: str, process_id: int = None, exc: Exception = None):
+        pass
+
+    @classmethod
     def execute(cls, params):
         results = []
         for param in params:
-            if isinstance(param,list):
-                r=[]
+            if isinstance(param, list):
+                r = []
                 for p in param:
-                    if p==-1:
+                    if p == -1:
                         raise Exception("Simulando error desde un proceso")
                     r.append(method(p))
                 results.append(r)
             else:
-                if param==-1:
+                if param == -1:
                     raise Exception("Simulando error desde un proceso")
-                if param==2:
+                if param == 2:
                     time.sleep(1)
-                if param==-2:
+                if param == -2:
                     for i in range(20000):
-                       results.append(method(i))
+                        results.append(method(i))
                     print("Termino tarea pesada")
                     continue
                 results.append(method(param))
         # print(results)
-        return results,None
+        return results, None
